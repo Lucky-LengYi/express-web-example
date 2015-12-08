c31270e5802b03920f0f63b5a929ceb34c1ec8c7
XTWang <meeting_love_lengyi@hotmail.com>
2015-12-09 01:19:16 +0800

    use ajax to add bookmarks

--- public/js/main.js
+++ public/js/main.js
@@ -14,11 +14,16 @@ $(function() {
   function insertItem(item) {
     var infoBox = '<div class="info-box"></div>';
     var info = '<div class="info">' + item.title + '</div>';
+    var deleteInfoBox = '<button type="button" class="btn btn-default del-button" data-id="' + item.id + '"  data-toggle="modal" data-target=".bs-example-modal-sm">删除</button>';
     var timestamp = '<div class="timestamp">' + formatDate(item.created) + '</div>';
 
     $infoBox = $(infoBox);
-    $infoBox.append(info + timestamp);
+    $infoBox.append(info + timestamp + deleteInfoBox);
     $('.main-info').append($infoBox);
+  }
+
+  function showBookmarksCount(count) {
+    $('.search-count').text('搜索到' + count + '条结果');
   }
 
   var bookmarks;
@@ -29,13 +34,16 @@ $(function() {
   }).done(function(data) {
     bookmarks = data;
     insertItems(data);
+    showBookmarksCount(bookmarks.length);
   });
 
   $('#keywords').on('input propertychange', function() {
     var that = this;
+    var count = 0;
     $('.main-info').empty();
     if (this.value === '') {
       insertItems(bookmarks);
+      showBookmarksCount(bookmarks.length);
       return;
     }
     bookmarks.forEach(function(item) {
@@ -46,10 +54,49 @@ $(function() {
         var span = '<span class="highlightingText">' + '$&' + '</span>';
         var result = {};
 
+        count ++;
         result.title = item.title.replace(condition, span);
         result.created = item.created;
         insertItem(result);
       }
     });
+    showBookmarksCount(count);
+  });
+
+  $('#confirm').on('click' ,function () {
+    $.ajax({
+      method: "post",
+      data: {
+        bookmarkName: $('#bookmarkName').val(),
+        bookmarkAddress: $('#bookmarkAddress').val()
+      },
+      url: "/bookmark",
+    }).done(function(data) {
+      bookmarks = data;
+      insertItems(data);
+      showBookmarksCount(bookmarks.length);
+      $('#myModal').modal('hide');
+    });
+  });
+
+  $('.main-info').on('click' ,function (evt) {
+    if (evt.target.type !== 'button') {
+      return;
+    }
+
+    var id = $(evt.target).data('id');
+    if (confirm("你确定删除该条信息？")) {
+
+      $.ajax({
+        method: "delete",
+        url: "/bookmarks?id=" + id
+      }).done(function(data) {
+        $('.main-info').empty();
+
+        bookmarks = data;
+        insertItems(data);
+        showBookmarksCount(bookmarks.length);
+      });
+    }
   });
 });
