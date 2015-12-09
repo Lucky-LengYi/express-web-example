910e94914126a7f4a1afd518c4a7f6d59e2149c7
XTWang <meeting_love_lengyi@hotmail.com>
2015-12-09 23:27:06 +0800

    add bookmarks address

--- bookmarks-helper.js
+++ bookmarks-helper.js
@@ -3,6 +3,7 @@ var bookmarks = require('./bookmarks.json');
 function bookmarksHelper() {
   bookmarks.forEach(function (item, i) {
     bookmarks[i].id = i;
+    bookmarks[i].address = '';
   });
 
   return bookmarks;
