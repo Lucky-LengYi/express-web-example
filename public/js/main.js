b3f23195290872d316e95406594b432071d6aaad
XTWang <meeting_love_lengyi@hotmail.com>
2015-12-09 23:28:06 +0800

    remove useless file

--- public/js/main.js
+++ public/js/main.js
@@ -14,6 +14,10 @@ $(function() {
   function insertItem(item) {
     var infoBox = '<div class="info-box"></div>';
     var info = '<div class="info">' + item.title + '</div>';
+
+    if (item.address !== '') {
+      info = '<a href="' + item.address + '">' + info + '</a>';
+    }
     var deleteInfoBox = '<button type="button" class="btn btn-default del-button" data-id="' + item.id + '"  data-toggle="modal" data-target=".bs-example-modal-sm">删除</button>';
     var timestamp = '<div class="timestamp">' + formatDate(item.created) + '</div>';
 
@@ -26,6 +30,30 @@ $(function() {
     $('.search-count').text('搜索到' + count + '条结果');
   }
 
+  function paging(argument) {
+    $(".info-box:gt(10)").hide();
+    var total = $(".info-box").length + 1;
+    var currentPage = 10;
+    var totalPage = Math.ceil(total / currentPage);
+
+    $(".tcdPageCode").createPage({
+      pageCount: totalPage,
+      current: 1,
+      backFn: function(p) {
+        $.each($(".info-box"), function(index, item) {
+          var start = currentPage * (p - 1);
+          var end = currentPage * p;
+          if (index >= start && index < end) {
+            $(this).show();
+          } else {
+            $(this).hide();
+          }
+        });
+      }
+    });
+  }
+
+
   var bookmarks;
 
   $.ajax({
@@ -35,6 +63,7 @@ $(function() {
     bookmarks = data;
     insertItems(data);
     showBookmarksCount(bookmarks.length);
+    paging();
   });
 
   $('#keywords').on('input propertychange', function() {
@@ -60,22 +89,34 @@ $(function() {
         insertItem(result);
       }
     });
+    paging();
     showBookmarksCount(count);
   });
 
   $('#confirm').on('click' ,function () {
+    var bookmarkName = $('#bookmarkName').val();
+    var bookmarkAddress = $('#bookmarkAddress').val();
+    if (bookmarkName === '' || bookmarkAddress === '') {
+      $('.message').text('请输入正确的书签名称/书签链接');
+      return false;
+    }
+    $('.message').text('');
+    $('#bookmarkName').val('');
+    $('#bookmarkAddress').val('');
     $.ajax({
       method: "post",
       data: {
-        bookmarkName: $('#bookmarkName').val(),
-        bookmarkAddress: $('#bookmarkAddress').val()
+        bookmarkName: bookmarkName,
+        bookmarkAddress: bookmarkAddress
       },
       url: "/bookmark",
     }).done(function(data) {
       bookmarks = data;
+      $('.main-info').empty();
       insertItems(data);
       showBookmarksCount(bookmarks.length);
       $('#myModal').modal('hide');
+      paging();
     });
   });
 
@@ -96,6 +137,7 @@ $(function() {
         bookmarks = data;
         insertItems(data);
         showBookmarksCount(bookmarks.length);
+        paging();
       });
     }
   });
